6559170662cb580f8c34ba755ec259a08d8d1531
chensongsong <chensongsong@geetest.com>
2020-12-21 11:09:36 +0800

    新增跳转设置页面

--- app/src/main/java/com/song/webview/MainActivity.kt
+++ app/src/main/java/com/song/webview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.song.webview
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -21,6 +23,17 @@ class MainActivity : AppCompatActivity() {
             requestWindowFeature(Window.FEATURE_NO_TITLE)
             setContentView(R.layout.activity_main)
             window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//            actionBar?.hide()
+//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+//                val lp  = window.attributes
+//                lp .layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+//                window.attributes = lp
+//            }
+//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+//                window.statusBarColor = Color.TRANSPARENT
+//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//            }
         } else {
             setContentView(R.layout.activity_main)
         }
--- app/src/main/java/com/song/webview/ui/home/HomeFragment.kt
+++ app/src/main/java/com/song/webview/ui/home/HomeFragment.kt
@@ -1,18 +1,17 @@
 package com.song.webview.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.preference.PreferenceManager
 import com.song.webview.R
 import com.song.webview.dialog.WebViewDialog
-import com.song.webview.utils.Constants
 
 class HomeFragment : Fragment(), View.OnClickListener {
 
@@ -25,15 +24,25 @@ class HomeFragment : Fragment(), View.OnClickListener {
     ): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val button: AppCompatButton = root.findViewById(R.id.text_home)
+        val button = root.findViewById<AppCompatButton>(R.id.btn_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             button.text = it
         })
         button.setOnClickListener(this)
+        root.findViewById<AppCompatButton>(R.id.btn_jump).setOnClickListener(this)
         return root
     }
 
-    override fun onClick(v: View?) {
-        WebViewDialog().show(parentFragmentManager, R.id.text_home.toString())
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_home -> WebViewDialog().show(parentFragmentManager, R.id.btn_home.toString())
+            R.id.btn_jump -> {
+                val intent = Intent()
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
+                intent.data = Uri.fromParts("package", v.context.packageName, null)
+                startActivity(intent)
+            }
+        }
     }
 }
